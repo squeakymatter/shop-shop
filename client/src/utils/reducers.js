49cf1f08f8ad5c52301d6b82c502fa800baa6cbe
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-05-08 17:41:02 -0700

    shopping cart

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,9 +1,15 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
-} from "./actions";
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -22,8 +28,62 @@ export const reducer = (state, action) => {
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
-        currentCategory: action.currentCategory
-      }
+        currentCategory: action.currentCategory,
+      };
+
+    case ADD_TO_CART:
+      return {
+        // ...state operator preserves everything else on state
+        ...state,
+        //  set `cartOpen` to true so users can immediately view cart with newly added item, if it's not already open
+        cartOpen: true,
+        //update `cart` property to add `action.product to end of array
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case REMOVE_FROM_CART:
+      // filter() method only keeps items that don't match the provided _id property
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+      // check `length` of the array to set `cartOpen` to `false` when the array is empty
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        // use map() here because original state is immutable.
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state, 
+        cartOpen: !state.cartOpen
+      }  
 
     default:
       return state;
@@ -31,5 +91,5 @@ export const reducer = (state, action) => {
 };
 
 export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState)
+  return useReducer(reducer, initialState);
 }
